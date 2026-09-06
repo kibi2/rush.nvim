b623dce482d4fa96d407c2ff5c0bdc988c1b1440
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-09-07 07:46:01 +0900

    feat: "j" - "j" interval < 200ms -> rush

--- lua/rush/init.lua
+++ lua/rush/init.lua
@@ -1,9 +1,19 @@
 local M = {}
 
 local count = 0
+local last_j = 0
 
 local function next_j()
-	count = count + 1
+	local now = vim.uv.hrtime()
+	local elapsed = (now - last_j) / 1e6
+
+	if elapsed > 200 then
+		count = 1
+	else
+		count = count + 1
+	end
+
+	last_j = now
 
 	if count == 1 then
 		return "j"
