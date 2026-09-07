2e2497b4b3f97c15fd98968d1436181abd9d43da
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-09-07 11:47:59 +0900

    feat: update rush function

--- lua/rush/init.lua
+++ lua/rush/init.lua
@@ -5,25 +5,69 @@ local rep_key = nil
 local rep_command = nil
 local rep_count = 0
 
--- TODO: count と連打間隔から加速量を決める
-local function rush(count)
-	if count == 1 then
-		return 1
-	elseif count == 2 then
-		return 2
-	elseif count == 3 then
-		return 4
-	elseif count == 4 then
-		return 8
+-- ユーザー設定:
+-- { 回数, rush倍率 }
+--
+-- 例:
+-- { 3, 2 } = 3回分をrush=2で移動
+-- { 3, 4 } = 次の3回分をrush=4で移動
+-- { 3, 8 } = 次の3回分をrush=8で移動
+local default_rush_steps = {
+	{ 3, 2 },
+	{ 3, 4 },
+	{ 3, 8 },
+}
+
+local rush_levels = {}
+
+local function remake_rush_table(rush_steps)
+	local result = {}
+	local count = 0
+	local rush = 1
+
+	for _, step in ipairs(rush_steps) do
+		count = count + step[1]
+
+		table.insert(result, {
+			count = count,
+			rush = rush,
+		})
+
+		rush = step[2]
 	end
-	return 8
+
+	table.insert(result, {
+		count = math.huge,
+		rush = rush,
+	})
+
+	return result
+end
+
+local function rush(count)
+	for _, v in ipairs(rush_levels) do
+		if count <= v.count then
+			return v.rush
+		end
+	end
+
+	return 1
+end
+
+function M.setup(opts)
+	opts = opts or {}
+
+	local rush_steps = opts.rush_steps or default_rush_steps
+	rush_levels = remake_rush_table(rush_steps)
 end
 
 local function on_key(key, typed)
 	if #typed == 0 then
 		return
 	end
+
 	-- print("on_key", tostring(key), vim.inspect(typed))
+
 	if state == "init" then
 		if typed == "g" then
 			state = "g"
@@ -55,19 +99,24 @@ vim.on_key(on_key)
 
 for _, motion in ipairs({ "h", "j", "k", "l" }) do
 	vim.keymap.set("n", motion, function()
-		-- print("keymap" .. motion)
 		if state ~= "repeat" or rep_key ~= motion then
 			return motion
 		end
+
 		local n_rep = rush(rep_count)
+
 		print(rep_count)
+
 		if n_rep == 1 then
 			return rep_command
 		end
+
 		return n_rep .. rep_command
 	end, {
 		expr = true,
 	})
 end
+
+M.setup()
 
 return M
